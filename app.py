1f42429178b8a29f8d4cef8734ac2b38687b02c8
tamduy15 <nguyentamduy9b@gmail.com>
2025-12-22 11:42:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import datetime, timedelta
 import requests
+import pytz
+from datetime import datetime
 
 # ---------------------------------------------------------
 # 1. KẾT NỐI API & CẤU HÌNH GIAO DIỆN
@@ -515,6 +517,7 @@ else:
             with col_ai:
                 st.markdown(render_ai_analysis(df, symbol), unsafe_allow_html=True)
         else: st.error(d["error"])
+
 
 
 
